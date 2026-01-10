c5114da6cd79580a436246ec4f3ce3fba7879d59
rohaanimtiaz <rohaanimtiax7@gmail.com>
2026-01-10 23:45:30 +0500

    Delete products from HTML when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,8 +1,8 @@
-export const cart = [{
-  id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+export let cart = [{
+  productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
   quantity: 2
 }, {
-  id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+  productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
   quantity: 1
 }];
 
@@ -14,11 +14,23 @@ export function addToCart(productId) {
       matchingItem = cartItem;
   });
 
-  if (matchingItem) matchingItem.quantity++;
+  if (matchingItem)
+    matchingItem.quantity += 1;
   else {
     cart.push({
       productId: productId,
       quantity: 1
     });
   }
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach(cartItem => {
+    if (cartItem.productId !== productId)
+      newCart.push(cartItem);
+  });
+
+  cart = newCart;
 }
