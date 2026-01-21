078f430963d88cbca9ba60b13f4523f4433807e5
rohaanimtiaz <rohaanimtiax7@gmail.com>
2026-01-21 23:27:16 +0500

    Create automated tests for addToCart

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,8 +1,8 @@
-import { cart, removeFromCart, updateDeliveryOption, updateCartQuantity } from '../../data/cart.js';
-import { getProduct } from '../../data/products.js';
+import { cart, removeFromCart, updateDeliveryOption, updateCartQuantity } from '../data/cart.js';
+import { getProduct } from '../data/products.js';
 import formatCurrency from '../utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
-import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
+import { deliveryOptions, getDeliveryOption } from '../data/deliveryOptions.js';
 import { renderPaymentSummary } from './paymentSummary.js';
 
 export function renderOrderSummary() {
