0962e5982e9fd0c0be26c6dfe08af4b5c1fd3d90
rohaanimtiaz <rohaanimtiax7@gmail.com>
2026-01-29 03:23:01 +0500

    Add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
+import '../scripts/data/backend-practice.js';
 
 renderOrderSummary();
 renderPaymentSummary();
